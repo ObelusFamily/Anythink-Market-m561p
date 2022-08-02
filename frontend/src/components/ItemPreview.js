d3fd45e4a1c1a9c36113fd3748bf58d44bf79c34
vinayak0127 <vinayakrajbusiness@gmail.com>
2022-08-02 20:37:04 +0530

    api fixed

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,11 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={"placeholder.png"}
+        src={item.image}
+        onError={(e) => {
+          e.target.onerror = null;
+          e.target.src = "placeholder.png";
+        }}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
